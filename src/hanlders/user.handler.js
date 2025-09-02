550fc269521cad7e265bb8e447b79bf3e2c00788
Hussain Ali <hussainalee@icloud.com>
2025-09-02 20:15:28 +0500

    feat: implement user registration with avatar upload handling

--- src/hanlders/user.handler.js
+++ src/hanlders/user.handler.js
@@ -1,9 +1,55 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const resgisterUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
+  const { fullName, email, username, password } = req.body;
+
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required.");
+  }
+
+  if (
+    await User.findOne({
+      $or: [{ username }, { email }],
+    })
+  ) {
+    throw new ApiError(409, "User with email or username already exists.");
+  }
+
+  // classical way of handling files.
+  if (
+    !(
+      req.files &&
+      Array.isArray(req.files.avatar) &&
+      req.files?.avatar?.length > 0
+    )
+  ) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const cloudinaryResponse = await uploadOnCloudinary(avatarLocalPath);
+
+  const user = await User.create({
+    username: username?.trim()?.toLowerCase(),
+    email,
+    password,
+    fullName,
+    avatar: cloudinaryResponse?.url || "",
   });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "user registed successfully."));
 });
 
 export { resgisterUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
 import { resgisterUser } from "../hanlders/user.handler.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(resgisterUser);
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+  ]),
+  resgisterUser
+);
 
 export default router;
