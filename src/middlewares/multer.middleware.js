4b98463717092e694138c6c922077157417b1060
Hussain Ali <hussainalee@icloud.com>
2025-09-02 20:14:01 +0500

    fix: correct file path in multer storage configuration

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/public/temp");
+    cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
     // for now, disabling renaming of file.
@@ -11,4 +11,4 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage });
+export const upload = multer({ storage });
