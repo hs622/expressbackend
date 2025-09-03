4ac6697a46f035379dfcf794b61aeb0b0fc78205
Hussain Ali <hussainalee@icloud.com>
2025-09-03 17:33:00 +0500

    feat: refactor user model to include nested profile, contact, and address schemas

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,81 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+
+const profile = new Schema({
+  firstName: {
+    type: String,
+    trim: true,
+    required: true,
+    lowecase: true,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    lowecase: true,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: Date,
+  },
+  gender: {
+    type: String,
+    enum: ["Male", "Female", "Other"],
+  },
+});
+
+const contact = new Schema({
+  countryCode: {
+    type: String,
+    minlength: 1,
+    maxlength: 4,
+    match: /^[0-9]+$/,
+  },
+  number: {
+    type: String,
+    minlength: 6,
+    maxlength: 15,
+  },
+  default: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const address = new Schema({
+  address: {
+    type: String,
+    trim: true,
+    lowecase: true,
+  },
+  city: {
+    type: String,
+    trim: true,
+    lowecase: true,
+  },
+  state: {
+    type: String,
+    trim: true,
+    lowecase: true,
+  },
+  country: {
+    type: String,
+    trim: true,
+    lowecase: true,
+  },
+  postcode: {
+    type: String,
+    trim: true,
+    lowecase: true,
+  },
+  timezone: {
+    type: String,
+    trim: true,
+  },
+});
 
 const userSchema = mongoose.Schema(
   {
@@ -12,16 +87,6 @@ const userSchema = mongoose.Schema(
       index: true,
       unique: true,
     },
-    fullName: {
-      type: String,
-      trim: true,
-      required: true,
-      lowecase: true,
-    },
-    avatar: {
-      type: String,
-      required: true,
-    },
     email: {
       type: String,
       required: true,
@@ -29,18 +94,39 @@ const userSchema = mongoose.Schema(
       lowercase: true,
       trim: true,
     },
+    password: {
+      type: String,
+      required: [true, "Password is required."],
+    },
+    role: {
+      type: Schema.Types.ObjectId,
+      ref: "Role",
+      default: null,
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive", "suspended"],
+      default: "active",
+    },
     watchHistory: [
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Video",
       },
     ],
-    password: {
-      type: String,
-      required: [true, "Password is required."],
-    },
     refreshToken: {
       type: String,
+    },
+
+    // nested objects
+    profile: {
+      type: profile,
+    },
+    address: {
+      type: address,
+    },
+    contact: {
+      type: contact,
     },
   },
   { timestamps: true }
@@ -48,8 +134,10 @@ const userSchema = mongoose.Schema(
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
+  console.log("password hashing..");
 
   this.password = await bcrypt.hash(this.password, 10);
+  console.log("password hashing completed..");
   next();
 });
 
@@ -63,7 +151,7 @@ userSchema.methods.generateAccessToken = function () {
       _id: this.id,
       email: this.email,
       username: this.username,
-      fullName: this.fullName,
+      firstName: this.profile.firstName,
     },
     process.env.ACCESS_TOKEN_SECRET,
     {
