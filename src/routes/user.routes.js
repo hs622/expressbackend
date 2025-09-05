9281b0cb2d5f1f6676fe5a813a9bac861a13c94a
Hussain Ali <hussainalee@icloud.com>
2025-09-05 17:37:58 +0500

    fix: correct HTTP methods for user routes and improve route organization

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,11 +5,13 @@ import {
   logoutUser,
   refreshAccessToken,
   resgisterUser,
-  updateUsername, 
+  updateUsername,
   updateProfileDetails,
   updateAvatarImage,
   fetchCurrentUser,
   updateContactDetails,
+  getWatchHistory,
+  getUserChannelProfile,
 } from "../hanlders/user.handler.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -21,14 +23,21 @@ router.route("/login").post(loginUser);
 router.route("/reissue-access-token").post(refreshAccessToken);
 
 // secure routes
-router.route("/get-current-user").post(verifyJWT, fetchCurrentUser);
+router.route("/get-current-user").get(verifyJWT, fetchCurrentUser);
+router.route("/history").get(verifyJWT, getWatchHistory);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+
 router.route("/logout").post(verifyJWT, logoutUser);
-router.route("/update-password").post(verifyJWT, updateCurrentPassword);
-router.route("/update-username").post(verifyJWT, updateUsername);
-router.route("/update-profile").post(verifyJWT, updateProfileDetails);
-router.route("/update-contact").post(verifyJWT, updateContactDetails);
+router.route("/update-password").patch(verifyJWT, updateCurrentPassword);
+router.route("/update-username").patch(verifyJWT, updateUsername);
+router.route("/update-profile").patch(verifyJWT, updateProfileDetails);
+router.route("/update-contact").patch(verifyJWT, updateContactDetails);
 router
   .route("/update-avatar")
-  .post(verifyJWT, upload.single("avatar"), updateAvatarImage);
+  .patch(verifyJWT, upload.single("avatar"), updateAvatarImage);
+
+// administrator route
+router.route("/suspend-status").post(verifyJWT);
+router.route("/suspend-account").post(verifyJWT);
 
 export default router;
